9b7f8868af7675dc650371d2f6d69336a57fb49d
--global <jvahe@live.com>
2016-11-26 20:59:12 -0500

    added Score entity. Needs doublechecking

---
+++ src/entities/Score.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class Score {
+
+    int star;
+    int numScore;
+
+    public Score() {
+        star = 0;
+        numScore = 0;
+    }
+
+    int getStar () {
+        return star;
+    }
+
+    int getScore () {
+        return numScore;
+    }
+
+    
+    void setStar (int n) {
+        star = n;
+    }
+
+    void setScore (int n) {
+        numScore = n;
+    }
+    
+    
+    
+    //Mistake in the UML doc?
+    int getStar (int n) {
+        return (star = n);
+    }
+
+    int getScore (int n) {
+        return (numScore = n);
+    }
+}
