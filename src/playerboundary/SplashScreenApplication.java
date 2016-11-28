63806061a0b5a4ea9add7cb9899d8fb2b89b5690
Goutham <Goutham@130.215.175.148>
2016-11-27 23:40:34 -0500

    Updated appearence of splash screen

--- src/playerboundary/SplashScreenApplication.java
+++ src/playerboundary/SplashScreenApplication.java
@@ -43,28 +43,28 @@ public class SplashScreenApplication extends JPanel {
         this.setLayout(null);
         
         lblNewLabel = new JLabel("Team Copper Presents\r\n");
-        lblNewLabel.setBounds(169, 66, 162, 52);
-        lblNewLabel.setFont(new Font("Bookman Old Style", Font.PLAIN, 14));
+        lblNewLabel.setBounds(230, 110, 367, 52);
+        lblNewLabel.setFont(new Font("Bookman Old Style", Font.PLAIN, 30));
         this.add(lblNewLabel);
         
         lblNewLabel_1 = new JLabel("John Dyer");
-        lblNewLabel_1.setBounds(204, 129, 88, 28);
+        lblNewLabel_1.setBounds(347, 241, 162, 28);
         this.add(lblNewLabel_1);
         
         lblNewLabel_3 = new JLabel("Annie Hernandez");
-        lblNewLabel_3.setBounds(204, 156, 95, 28);
+        lblNewLabel_3.setBounds(347, 268, 162, 28);
         this.add(lblNewLabel_3);
         
         lblNewLabel_4 = new JLabel("Anthony Gringeri");
-        lblNewLabel_4.setBounds(204, 182, 88, 28);
+        lblNewLabel_4.setBounds(347, 294, 162, 28);
         this.add(lblNewLabel_4);
         
         lblNewLabel_5 = new JLabel("James Kradjian");
-        lblNewLabel_5.setBounds(204, 206, 82, 28);
+        lblNewLabel_5.setBounds(347, 318, 162, 28);
         this.add(lblNewLabel_5);
         
         lblNewLabel_2 = new JLabel("Goutham Deva");
-        lblNewLabel_2.setBounds(204, 237, 89, 19);
+        lblNewLabel_2.setBounds(347, 349, 162, 19);
         this.add(lblNewLabel_2);
         
     }
