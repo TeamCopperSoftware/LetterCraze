6e30d8e73667256282e7946b3cbea7fdfaa279ed
John Dyer <johndyer24@gmail.com>
2016-12-01 15:45:07 -0500

    Created Map class, edited Model class

--- src/entities/Model.java
+++ src/entities/Model.java
@@ -1,9 +1,18 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Model {
 	
-	public Model() {
-		
+	Map mainLevels; // main levels are stored in a map in order to keep track of player's progress
+	ArrayList<LevelModel> customLevels; // custom levels are always unlocked so they can be stored in an ArrayList
+	
+	public Model() {		
+		// creates default levels and organizes them into a Map
+	}
+	
+	public void importCustomLevels() {
+		// looks for custom level files, creates Level objects, adds them to list
 	}
 
 }
