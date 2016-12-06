fcb8b1570c3a1376781f9f0dc9d2a7820f9ab2ef
jvahe <jvahe@live.com>
2016-12-06 13:24:46 -0500

    added TODO

--- src/entities/Map.java
+++ src/entities/Map.java
@@ -3,37 +3,38 @@ package entities;
 import java.util.ArrayList;
 
 public class Map {
-	
-	ArrayList<LevelModel> levels;
-	int numUnlockedLevels; // probably should keep track of how many levels are locked/unlocked
-	
-	// this is the constructor used when creating Map object in Model class
-	public Map() {
-		levels = new ArrayList<LevelModel>();
-	}
-	
-	// probably don't need this constructor
-	public Map(ArrayList<LevelModel> levels) {
-		this.levels = levels;
-	}
-	
-	public void addLevel(LevelModel level) {
-		levels.add(level);
-	}
-	
-	public void removeLevel(int levelNumber) {
-		levels.remove(levelNumber);
-	}
-	
-	public boolean unlockNextLevel() {
-		// 1) check whether conditions are met for the next locked level to be unlocked
-		// 2) if they are met unlock the level and update numUnlocked levels
-		
-		return false;
-	}
-	
-	public ArrayList<LevelModel> getLevels() {
-		return levels;
-	}
+
+    ArrayList<LevelModel> levels;
+    int numUnlockedLevels; // probably should keep track of how many levels are locked/unlocked
+
+    // this is the constructor used when creating Map object in Model class
+    public Map() {
+        levels = new ArrayList<LevelModel>();
+    }
+
+    // probably don't need this constructor
+    public Map(ArrayList<LevelModel> levels) {
+        this.levels = levels;
+    }
+
+    public void addLevel(LevelModel level) {
+        levels.add(level);
+    }
+
+    public void removeLevel(int levelNumber) {
+        levels.remove(levelNumber);
+    }
+
+    public boolean unlockNextLevel() {
+        //TODO
+        // 1) check whether conditions are met for the next locked level to be unlocked
+        // 2) if they are met unlock the level and update numUnlocked levels
+
+        return false;
+    }
+
+    public ArrayList<LevelModel> getLevels() {
+        return levels;
+    }
 
 }
