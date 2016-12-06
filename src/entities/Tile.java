bfc65d8cc5d661fed243be9d022cb34833a54598
jvahe <jvahe@live.com>
2016-12-06 13:06:38 -0500

    fixed tile class

--- src/entities/Tile.java
+++ src/entities/Tile.java
@@ -1,27 +1,22 @@
 package entities;
 
 public class Tile {
-    
+
     String letter;
     int value;
-    
+
     public Tile (String letter, int value) {
         this.letter = letter;
         this.value = value;
     }
-    
+
     public String toString () {
         return letter;
     }
 
-	public String getTile() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int getPoints() {
-		// TODO Auto-generated method stub
-		return value;
-	}
+    public int getPoints() {
+        // TODO Auto-generated method stub
+        return value;
+    }
 }
 
