4458025d8dd9046de63b88e5388e2dc390ff5b66
John Dyer <johndyer24@gmail.com>
2016-12-11 13:22:13 -0500

    A few fixes for controllers for custom levels

--- src/playercontroller/AddWordController.java
+++ src/playercontroller/AddWordController.java
@@ -4,9 +4,13 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+
 import entities.*;
 import playerboundary.Application;
 import playerboundary.LevelApplication;
+import playerboundary.MapApplication;
+import playerboundary.ViewCustomLevelsApplication;
 
 /**
  * Controls all actions to do with mouse events over the Add Word Button in all levels.
@@ -17,6 +21,7 @@ public class AddWordController implements ActionListener {
 	Application topLevelApp;
 	LevelApplication level;
 	public LevelModel model;
+	public JPanel exitView;
 
 	/**
 	 * Provides all actions to add word button. Adds words to lists that were found.
@@ -53,9 +58,14 @@ public class AddWordController implements ActionListener {
 					// return to mainmenu
 					System.out.println("Score: " + thisLevel.getBestScore().getScore());
 					System.out.println("Stars: " + thisLevel.getBestScore().getStar());
-					topLevelApp.getModel().getMainLevels().unlockNextLevel(); //possibly unlock next level
-					topLevelApp.getMapApplication().refreshPanel();
-					topLevelApp.setContentPane(topLevelApp.getMapApplication());
+					if (exitView instanceof MapApplication) {
+						topLevelApp.getModel().getMainLevels().unlockNextLevel(); //possibly unlock next level
+						((MapApplication)exitView).refreshPanel();
+					}
+					else {
+						((ViewCustomLevelsApplication)exitView).refreshPanel();
+					}
+					topLevelApp.setContentPane(exitView);
 				}
 			}
 
@@ -75,9 +85,14 @@ public class AddWordController implements ActionListener {
 					// return to main menu
 					System.out.println("Score: " + thisLevel.getBestScore().getScore());
 					System.out.println("Stars: " + thisLevel.getBestScore().getStar());
-					topLevelApp.getModel().getMainLevels().unlockNextLevel(); //possibly unlock next level
-					topLevelApp.getMapApplication().refreshPanel();
-					topLevelApp.setContentPane(topLevelApp.getMapApplication());
+					if (exitView instanceof MapApplication) {
+						topLevelApp.getModel().getMainLevels().unlockNextLevel(); //possibly unlock next level
+						((MapApplication)exitView).refreshPanel();
+					}
+					else {
+						((ViewCustomLevelsApplication)exitView).refreshPanel();
+					}
+					topLevelApp.setContentPane(exitView);
 				}
 			}
 
@@ -99,8 +114,9 @@ public class AddWordController implements ActionListener {
 	 * @param topLevelApp Application, level LevelApplication, model LevelModel
 	 */
 	
-	public AddWordController(Application topLevelApp, LevelApplication level, LevelModel model) {
+	public AddWordController(Application topLevelApp, JPanel exitView, LevelApplication level, LevelModel model) {
 		this.topLevelApp = topLevelApp;
+		this.exitView = exitView;
 		this.level = level;
 		this.model = model;
 	}
