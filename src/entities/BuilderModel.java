cb46dbecd4f0f4cba31505a9de902395d3589e64
jvahe <jvahe@live.com>
2016-12-14 17:30:08 -0500

    fixed BuilderModel docs

--- src/entities/BuilderModel.java
+++ src/entities/BuilderModel.java
@@ -8,28 +8,32 @@ import java.util.ArrayList;
  */
 
 public class BuilderModel implements Serializable {
-	
-	/** All levels that are saved. */
-	ArrayList<LevelModel> savedLevels;
-	
-	/** 
-	 * Constructs Builder Model.
-	 */	
-	public BuilderModel() {
-		savedLevels = new ArrayList<LevelModel>();
-		
-	}
-	
-	/** 
-	 * Gets information for Saved Levels.
-	 */	
-	public ArrayList<LevelModel> getSavedLevels() {
-		return savedLevels;
-	}
-	
-	public void setSavedLevels(ArrayList<LevelModel> levels) {
-		savedLevels = levels;
-	}
+
+    /** All levels that are saved. */
+    ArrayList<LevelModel> savedLevels;
+
+    /** 
+     * Constructs Builder Model.
+     */	
+    public BuilderModel() {
+        savedLevels = new ArrayList<LevelModel>();
+
+    }
+
+    /** 
+     * Gets information for Saved Levels.
+     */	
+    public ArrayList<LevelModel> getSavedLevels() {
+        return savedLevels;
+    }
+
+    /** 
+     * Sets the information for Saved Levels.
+     * @param levels        ArrayList of the LevelModels to be used in the builder
+     */
+    public void setSavedLevels(ArrayList<LevelModel> levels) {
+        savedLevels = levels;
+    }
 
 
 }
