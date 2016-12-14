b0d8717775a61461129e86c94987342b253ae860
jvahe <jvahe@live.com>
2016-12-14 17:28:13 -0500

    fixed Board docs

--- src/entities/Board.java
+++ src/entities/Board.java
@@ -8,209 +8,198 @@ import java.io.Serializable;
 
 public class Board implements Serializable {
 
-	//Hashtable<Position, Square> squares;
-	Square[][] squares;
-	Word currentWord;  //TODO: Never used anywhere???
+    Square[][] squares;
+    Word currentWord; 
 
-	public Board () {
-		
-	}
-	
-	/** 
-	 * Construct board capable of retaining value in squares.
-	 * 
-	 * Initial value is given.
-	 * @param squares    36 valid tiles for all level boards
-	 */
+    public Board () {
 
-	public Board (Square[] squares) {
+    }
 
-		this.squares = new Square[6][6];
+    /** 
+     * Construct board capable of retaining value in squares.
+     * 
+     * Initial value is given.
+     * @param squares    36 valid tiles for all level boards
+     */
 
-		for (int n = 0; n < squares.length; n++) {
-			this.squares[squares[n].position().getX()][squares[n].position().getY()] = squares[n];
-		}
-	}
+    public Board (Square[] squares) {
 
-	/**
-	 * Makes new position of Square.
-	 * 
-	 * @param x    x position of square
-	 * @param y    y position of square
-	 */
-	
-	public Square makeSquare (int x, int y) {
-		//TODO Need a check to ensure new pos is valid
-		return makeSquare (new Position(x, y));
-	}
+        this.squares = new Square[6][6];
 
-	/**
-	 * Makes a square appear on the board
-	 * 
-	 * @param pos    Position of Square
-	 */
-	
-	public Square makeSquare (Position pos) {
-		//TODO Need a check to ensure new pos is valid
-		return new Square(pos, true);
-	}
+        for (int n = 0; n < squares.length; n++) {
+            this.squares[squares[n].position().getX()][squares[n].position().getY()] = squares[n];
+        }
+    }
 
-	/**
-	 * Looks up position of Square.
-	 * 
-	 * @param x    x position of square
-	 * @param y    y position of square
-	 */
-	
-	
-	public Square lookUpSquare (int x, int y) {
-		return squares[x][y];
-	}
-	
-	/**
-	 * Looks up if square should appear on board with position
-	 * 
-	 * @param pos    Position of Square
-	 */
+    /**
+     * Makes new position of Square.
+     * 
+     * @param x    x position of square
+     * @param y    y position of square
+     */
 
-	public Square lookUpSquare (Position pos) {
-		return lookUpSquare(pos.getX(), pos.getY());
-	}
+    public Square makeSquare (int x, int y) {
+        //TODO Need a check to ensure new pos is valid
+        return makeSquare (new Position(x, y));
+    }
 
-	/**
-	 * Removes all tiles that formed valid word on the board once added
-	 * 
-	 * @param w    Selected group of tiles that form a valid word
-	 */
-	
-	public void removeWord (Word w) {
-		for (int i = 0; i < w.getSquares().size(); i++) {
-			removeTile(w.getSquares().get(i));
-		}
-	}
+    /**
+     * Makes a square appear on the board
+     * 
+     * @param pos    Position of Square
+     */
 
-	/**
-	 * Floats tiles up and repopulates empty squares with random generated tiles.
-	 * @param sq   Most recent square selected
-	 */
-	
-	public Tile removeTile (Square sq) {
-		return sq.tilePop();
-	}
+    public Square makeSquare (Position pos) {
+        //TODO Need a check to ensure new pos is valid
+        return new Square(pos, true);
+    }
 
-	/**
-	 * Floats tiles up and repopulates empty squares with random generated tiles.
-	 */
-	
-	public void repopulate () {
-		// Iterate over every square on board (except bottom row)
-		// don't need to check bottom squares because we know they don't have squares beneath them to take tiles from
-		for (int y = 0; y < 5; y++) {
-			for (int x = 0; x < 6; x++) {
-				// if square is enabled but doesn't have tile iterate over the squares underneath it
-				if (squares[x][y].enabled && !squares[x][y].hasTile()) {
-					for (int y2 = y+1; y2 < 6; y2++) {
-						// if lower square is enabled and has a tile, remove it and add it to 1st square
-						if(squares[x][y2].enabled && squares[x][y2].hasTile()) {
-							Tile t = squares[x][y2].tilePop();
-							squares[x][y].tileAdd(t);
-							break;
-						}
-					}
-				}
-			}
-		}
+    /**
+     * Looks up position of Square.
+     * 
+     * @param x    x position of square
+     * @param y    y position of square
+     */
 
-		// Iterate over every square on board
-		for (int y = 0; y < 6; y++) {
-			for (int x = 0; x < 6; x++) {
-				// if square is enabled and doesn't have a tile generate a new one
-				if (squares[x][y].enabled && !squares[x][y].hasTile()) {
-					squares[x][y].generateRandomTile();
-				}
-			}
-		}
-	}
 
-	public Word getWord() {
-		return currentWord;
-	}
+    public Square lookUpSquare (int x, int y) {
+        return squares[x][y];
+    }
 
-	public void setWord(Word word) {
-		this.currentWord = word;
-	}
+    /**
+     * Looks up if square should appear on board with position
+     * 
+     * @param pos    Position of Square
+     */
 
-	/**
-	 * Generates a new tile for each square on the board.
-	 */
-	
-	public void reset() {
-		// Iterate over every square on board
-		for (int y = 0; y < 6; y++) {
-			for (int x = 0; x < 6; x++) {
-				if (squares[x][y].enabled) {
-					squares[x][y].generateRandomTile();	
-				}
-			}
-		}
-	}
-	
-	/**
-	 * Resets the board to the board given by b.
-	 * Used for themeLevels
-	 * @param b
-	 */
-	public void resetToBoard(Board b) {
-		// Iterate over every square on board
-				for (int y = 0; y < 6; y++) {
-					for (int x = 0; x < 6; x++) {
-						if (squares[x][y].enabled) {
-							squares[x][y].tileAdd(b.lookUpSquare(x, y).getTile());
-						}
-					}
-				}
-	}
+    public Square lookUpSquare (Position pos) {
+        return lookUpSquare(pos.getX(), pos.getY());
+    }
 
-	/** 
-	 * Just float the tiles up (used for theme levels).
-	 */
-	public void justFloatUp() {
-		// Iterate over every square on board (except bottom row)
-		// don't need to check bottom squares because we know they don't have squares beneath them to take tiles from
-		for (int y = 0; y < 5; y++) {
-			for (int x = 0; x < 6; x++) {
-				// if square is enabled but doesn't have tile iterate over the squares underneath it
-				if (squares[x][y].enabled && !squares[x][y].hasTile()) {
-					for (int y2 = y+1; y2 < 6; y2++) {
-						// if lower square is enabled and has a tile, remove it and add it to 1st square
-						if(squares[x][y2].enabled && squares[x][y2].hasTile()) {
-							Tile t = squares[x][y2].tilePop();
-							squares[x][y].tileAdd(t);
-							break;
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	/* Possible method that could be called when undoing move, currently unfinished
-	public void putWordBack(Word w) {
-		int wordSize = w.getSquares().size();
-		for (int i = 0; i < wordSize; i++) {
-			Square oldSquare = w.getSquares().get(i);
-			Square currentSquare = squares[oldSquare.position.getX()][oldSquare.position.getY()];
-			Tile t = currentSquare.tilePop();
-			currentSquare.tileAdd(oldSquare.tilePop());
-			for (int j = currentSquare.position.getY()+1; j < 5; j++) {
-				Square s = squares[currentSquare.position.getX()][j];
-				s.tileAdd(t);
-				t = squares[currentSquare.position.getX()][j+1].tilePop();
-			}
-			
-			
-		}
-	}
-	*/
+    /**
+     * Removes all tiles that formed valid word on the board once added
+     * 
+     * @param w    Selected group of tiles that form a valid word
+     */
+
+    public void removeWord (Word w) {
+        for (int i = 0; i < w.getSquares().size(); i++) {
+            removeTile(w.getSquares().get(i));
+        }
+    }
+
+    /**
+     * Floats tiles up and repopulates empty squares with random generated tiles.
+     * @param sq   Most recent square selected
+     */
+
+    public Tile removeTile (Square sq) {
+        return sq.tilePop();
+    }
+
+    /**
+     * Floats tiles up and repopulates empty squares with random generated tiles.
+     */
+
+    public void repopulate () {
+        // Iterate over every square on board (except bottom row)
+        // don't need to check bottom squares because we know they don't have squares beneath them to take tiles from
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 6; x++) {
+                // if square is enabled but doesn't have tile iterate over the squares underneath it
+                if (squares[x][y].enabled && !squares[x][y].hasTile()) {
+                    for (int y2 = y+1; y2 < 6; y2++) {
+                        // if lower square is enabled and has a tile, remove it and add it to 1st square
+                        if(squares[x][y2].enabled && squares[x][y2].hasTile()) {
+                            Tile t = squares[x][y2].tilePop();
+                            squares[x][y].tileAdd(t);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Iterate over every square on board
+        for (int y = 0; y < 6; y++) {
+            for (int x = 0; x < 6; x++) {
+                // if square is enabled and doesn't have a tile generate a new one
+                if (squares[x][y].enabled && !squares[x][y].hasTile()) {
+                    squares[x][y].generateRandomTile();
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the current word
+     */
+
+    public Word getWord() {
+        return currentWord;
+    }
+
+    /**
+     * Sets the current word
+     * @param word  current word
+     */
+
+    public void setWord(Word word) {
+        this.currentWord = word;
+    }
+
+    /**
+     * Generates a new tile for each square on the board.
+     */
+
+    public void reset() {
+        // Iterate over every square on board
+        for (int y = 0; y < 6; y++) {
+            for (int x = 0; x < 6; x++) {
+                if (squares[x][y].enabled) {
+                    squares[x][y].generateRandomTile();	
+                }
+            }
+        }
+    }
+
+    /**
+     * Resets the board to the board given by b.
+     * Used for themeLevels
+     * @param b    board to be reset to
+     */
+    public void resetToBoard(Board b) {
+        // Iterate over every square on board
+        for (int y = 0; y < 6; y++) {
+            for (int x = 0; x < 6; x++) {
+                if (squares[x][y].enabled) {
+                    squares[x][y].tileAdd(b.lookUpSquare(x, y).getTile());
+                }
+            }
+        }
+    }
+
+    /** 
+     * Just float the tiles up (used for theme levels).
+     */
+    public void justFloatUp() {
+        // Iterate over every square on board (except bottom row)
+        // don't need to check bottom squares because we know they don't have squares beneath them to take tiles from
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 6; x++) {
+                // if square is enabled but doesn't have tile iterate over the squares underneath it
+                if (squares[x][y].enabled && !squares[x][y].hasTile()) {
+                    for (int y2 = y+1; y2 < 6; y2++) {
+                        // if lower square is enabled and has a tile, remove it and add it to 1st square
+                        if(squares[x][y2].enabled && squares[x][y2].hasTile()) {
+                            Tile t = squares[x][y2].tilePop();
+                            squares[x][y].tileAdd(t);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
