96cc2e66939dad5403365ec7f922b27c6c95df8e
Annie <achernandez@wpi.edu>
2016-12-13 20:43:16 -0500

    src folder back up to 81%

--- test/buildercontroller/TestLevelButtonController.java
+++ test/buildercontroller/TestLevelButtonController.java
@@ -24,5 +24,19 @@ public class TestLevelButtonController extends TestCase {
 		lbc.actionPerformed(ae0);
 		lbc.actionPerformed(ae0);
 	}
+	
+	public void testNavigation2() {
+		BuilderModel model = new BuilderModel();
+		Application app = new Application(model);
+		CreateNewLevelApplication level = new CreateNewLevelApplication(model);
+		
+		JButton butt0 = level.getSquareButtons()[2][2];
+		JComboBox box0 = level.getLetterBoxes()[2][2];
+		level.setLevelType("Theme");
+		LevelButtonController lbc = new LevelButtonController(model, app, butt0, box0, -1);
+		ActionEvent ae0 = new ActionEvent(butt0, (int) ActionEvent.MOUSE_EVENT_MASK, "click butt");
+		lbc.actionPerformed(ae0);
+		lbc.actionPerformed(ae0);
+	}
 
 }
